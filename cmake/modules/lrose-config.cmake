2ad8a7e32fae2144f7eb05470cd5ca52a973e558
mike-dixon <dixon@ucar.edu>
2021-01-18 13:59:27 -0700

    Adding makeTdrpParams() function

--- cmake/modules/lrose-config.cmake
+++ cmake/modules/lrose-config.cmake
@@ -9,21 +9,27 @@ endif(NOT LROSE_PREFIX)
 
 set(LROSE_LIBRARIES kd tdrp Radx Ncxx toolsa dataport netcdf hdf5_cpp hdf5 z)
 
-# Add a custom generator for TDRP Params.cc and Params.hh files
-# from their associated paramdef.<app> file
+# Function for creating TDRP Params.cc and Params.hh files
 
-find_program(TDRP_EXECUTABLE tdrp_gen PATHS ${LROSE_PREFIX}/bin /usr/local/lrose/bin)
+function(makeTdrpParams)
 
-add_custom_command (
-  OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Params.hh ${CMAKE_CURRENT_SOURCE_DIR}/Params.cc
-  DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/paramdef.${PROJECT_NAME}
-  COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} && ${TDRP_EXECUTABLE}
-  -c++
-  -f paramdef.${PROJECT_NAME}
-  -prog ${PROJECT_NAME}
-  -add_ncar_copyright
-  COMMENT "Generating/updating Params.hh and Params.cc for ${PROJECT_NAME}"
-  )
+  # Add a custom generator for TDRP Params.cc and Params.hh files
+  # from their associated paramdef.<app> file
+
+  find_program(TDRP_EXECUTABLE tdrp_gen PATHS ${LROSE_PREFIX}/bin /usr/local/lrose/bin)
+  
+  add_custom_command (
+    OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Params.hh ${CMAKE_CURRENT_SOURCE_DIR}/Params.cc
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/paramdef.${PROJECT_NAME}
+    COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} && ${TDRP_EXECUTABLE}
+    -c++
+    -f paramdef.${PROJECT_NAME}
+    -prog ${PROJECT_NAME}
+    -add_ncar_copyright
+    COMMENT "Generating/updating Params.hh and Params.cc for ${PROJECT_NAME}"
+    )
+
+endFunction()
 
 message(STATUS "lrose-config: CMAKE_INSTALL_PREFIX: " ${CMAKE_INSTALL_PREFIX})
 message(STATUS "lrose-config: LROSE_PREFIX: " ${LROSE_PREFIX})
